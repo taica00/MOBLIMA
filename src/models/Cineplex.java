4ac100ff9d4c5a46a23797b93a7ebfb12b5de425
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-17 03:41:29 +0800

    getter, getCinema

--- src/models/Cineplex.java
+++ src/models/Cineplex.java
@@ -1,7 +1,28 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Cineplex {
-    List<Cinema> cinemas;
+public class Cineplex implements java.io.Serializable {
+    private String name;
+    private List<Cinema> cinemas;
+
+    public Cineplex(String name, Cinema[] cinemas) {
+        this.name = name;
+        this.cinemas = new ArrayList<>(Arrays.asList(cinemas));
+    }
+    
+    public Cinema getCinema(String location) {
+        for (Cinema cinema : cinemas) {
+            if (location.equals(cinema.getLocation()))
+                return cinema;
+        }
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
 }
