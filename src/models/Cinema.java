bda5f2902fbe8c4a9234061a4a8e0dc71e333b83
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-17 03:40:55 +0800

    getters, addSession, getShowTimes

--- src/models/Cinema.java
+++ src/models/Cinema.java
@@ -1,7 +1,36 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Cinema {
-    List<Session> showTimes;
+public class Cinema implements java.io.Serializable {
+    private String cineplex;
+    private String location;
+    private List<Session> showTimes;
+
+    public Cinema(String cineplex, String location) {
+        this.cineplex = cineplex;
+        this.location = location;
+        showTimes = new ArrayList<>();
+    }
+
+    public void addSession(Movie movie, String date, String time, String cinemaType) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy hh:mma");
+        LocalDateTime dateTime = LocalDateTime.parse(date + " " + time, formatter);
+        showTimes.add(new Session(this, movie, dateTime, cinemaType));
+    }
+
+    public List<Session> getShowTimes() {
+        return showTimes;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCineplex() {
+        return cineplex;
+    }
 }
