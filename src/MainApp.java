546ecc7fc9abe6e895ea21e4d2376f678a644f2f
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-17 14:50:17 +0800

    still in test stage

---
+++ src/MainApp.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+import models.Cineplex;
+import models.Movie;
+import models.Session;
+
+public class MainApp {
+    public static void main(String[] args) {
+        List<Movie> movies = null;
+        Cineplex[] cineplexes = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("src/data/movies.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            movies = (List<Movie>) in.readObject();
+            in.close();
+            fileIn.close();
+
+            fileIn = new FileInputStream("src/data/cineplexesshowtimes.ser");
+            in = new ObjectInputStream(fileIn);
+            cineplexes = (Cineplex[])in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
+        List<Session> sessions = cineplexes[1].getCinema("Funan").getShowTimes();
+        for (Session session : sessions)
+            System.out.println(session);
+
+    }
+}
