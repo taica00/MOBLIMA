4b2ce767b0230512972d29ae1e5c55dfd467a395
taica00 <taichenan00@gmail.com>
2022-10-20 00:39:25 +0800

    print out seating for the session

---
+++ src/models/Seating.java
@@ -0,0 +1,90 @@
+package models;
+
+import models.Session.CinemaClass;
+
+public class Seating implements java.io.Serializable {
+    private int[][] seats;
+
+    public Seating(CinemaClass cinemaClass) {
+        generateSeating(cinemaClass);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
+        for (int i = 0; i < seats[0].length-3; i++) {
+            if (i == (seats[0].length-5)/2)
+                sb.append("-----SCREEN-");
+            else
+                sb.append("---");
+        }
+        sb.append("--\n\n");
+        for (int i = 0; i < seats.length; i++) {  
+            sb.append((char)(i+'A') + " ");  // index
+            for (int j = 0; j < seats[i].length; j++) { //seats
+                if (seats[i][j] == 0)
+                    sb.append("[ ]");
+                else if (seats[i][j] == 1)
+                    sb.append("[X]");
+                else
+                    sb.append("   ");
+            }
+            sb.append(" " + (char)(i+'A') + "\n"); // index
+        }
+        sb.append("  ");
+        for (int i = 0, j = 1; i < seats[0].length; i++) { // index
+            if (seats[0][i] == -1)
+                sb.append("   ");
+            else { 
+                if (j >= 10)
+                    sb.append(j + " ");
+                else
+                    sb.append(" " + j + " ");
+                j++;
+            }
+        }
+        sb.append("\nLegend: [ ]Available  [X]Occupied\n"); // legend
+        return sb.toString();
+    }
+
+    private void generateSeating(CinemaClass cinemaClass) {
+        int rows = 0, columns = 0;
+        int[] aisles;
+        switch (cinemaClass) {
+            case STANDARD: 
+                rows = 17;
+                columns = 36;
+                aisles = new int[]{7, 28};
+                break;
+            case GVMAX: 
+                rows = 10;
+                columns = 20;
+                aisles = new int[]{4, 15};
+                break;
+            case GOLDCLASS:
+            case GOLDCLASSEXPRESS:
+                rows = 4;
+                columns = 11;
+                aisles = new int[]{2, 5, 8};
+                break;
+            case DELUXEPLUS:
+                rows = 5;
+                columns = 11;
+                aisles = new int[]{2, 5, 8};
+                break;
+            case GEMINI: //each index represents 2 seats
+                rows = 7;
+                columns = 11;
+                aisles = new int[]{2, 5, 8};
+                break;
+            default: aisles = new int[1]; 
+        }
+        seats = new int[rows][columns];
+        for (int i : aisles) {
+            for (int j = 0; j < seats.length; j++) {
+                seats[j][i] = -1; //aisle
+            }
+        }
+    }
+}
--- src/models/Session.java
+++ src/models/Session.java
@@ -1,25 +1,24 @@
 package models;
 
 import java.time.LocalDateTime;
-import java.util.Random;
 
 public class Session implements java.io.Serializable {
     private Cinema cinema;
     private Movie movie;
     private LocalDateTime dateTime;
-    private int[][] seating;
     private CinemaClass cinemaClass;
+    private Seating seating;
     private String movieType;
 
-    enum CinemaClass {STANDARD, GVMAX, GOLDCLASS, GOLDCLASSEXPRESS, DELUXEPLUS, GEMINI}
+    public enum CinemaClass {STANDARD, GVMAX, GOLDCLASS, GOLDCLASSEXPRESS, DELUXEPLUS, GEMINI}
 
     public Session(Cinema cinema, Movie movie, LocalDateTime dateTime, String cinemaClass) {
         this.cinema = cinema;
         this.movie = movie;
         this.dateTime = dateTime;
         this.cinemaClass = CinemaClass.valueOf(cinemaClass);
+        seating = new Seating(this.cinemaClass);
         movieType = "2D";
-        generateSeating();
     }
 
     @Override
@@ -31,53 +30,54 @@ public class Session implements java.io.Serializable {
         return sb.toString();
     }
 
-    private void generateSeating() {
-        int rows = 0, columns = 0;
-        int[] aisles;
-        switch (cinemaClass) {
-            case STANDARD: 
-                rows = 17;
-                columns = 36;
-                aisles = new int[]{8, 29};
-                break;
-            case GVMAX: 
-                rows = 10;
-                columns = 20;
-                aisles = new int[]{4, 15};
-                break;
-            case GOLDCLASS:
-            case GOLDCLASSEXPRESS:
-                rows = 4;
-                columns = 11;
-                aisles = new int[]{2, 5, 8};
-                break;
-            case DELUXEPLUS:
-                rows = 5;
-                columns = 11;
-                aisles = new int[]{2, 5, 8};
-                break;
-            case GEMINI: //each index represents 2 seats
-                rows = 7;
-                columns = 8;
-                aisles = new int[]{1, 3, 5};
-                break;
-            default: aisles = new int[1]; 
-        }
-        seating = new int[rows][columns];
-        for (int i : aisles) {
-            for (int j = 0; j < seating.length; j++) {
-                seating[j][i] = -1; //aisle
-            }
-        }
-        Random rd = new Random();
-        int takenSeats = rd.nextInt(seating.length*seating[0].length/2);
-        for (int i = 0; i < takenSeats; i++) {
-            int x, y;
-            do {
-                x = rd.nextInt(seating.length);
-                y = rd.nextInt(seating[0].length);
-            } while (seating[x][y] != 0);
-            seating[x][y] = 1;
-        }
-    } 
+    public Cinema getCinema() {
+        return cinema;
+    }
+
+    public void setCinema(Cinema cinema) {
+        this.cinema = cinema;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public CinemaClass getCinemaClass() {
+        return cinemaClass;
+    }
+
+    public void setCinemaClass(CinemaClass cinemaClass) {
+        this.cinemaClass = cinemaClass;
+    }
+
+    public Seating getSeating() {
+        return seating;
+    }
+
+    public void setSeating(Seating seating) {
+        this.seating = seating;
+    }
+
+    public String getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.movieType = movieType;
+    }
+
+    
+
 }
