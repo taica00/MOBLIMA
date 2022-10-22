0ee600a7db1c1b1c0fe15d7912488e1398140814
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-23 04:10:18 +0800

    listMovies, searchMovies

--- src/main/controllers/MovieController.java
+++ src/main/controllers/MovieController.java
@@ -3,30 +3,33 @@ package main.controllers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import main.models.Movie;
-import main.ui.MovieDetailsUI;
 import main.ui.MovieListUI;
 
 public class MovieController extends Controller {
     private static List<Movie> movies;
     private static final String FILENAME = "src/main/data/movies.ser";
 
-    public static void listMovies(String movieName) {
-        int i = 1;
-        for (Movie movie : movies) {
-            System.out.println((i++) + ". " + movie.getTitle());
-        }
-        System.out.println();
-        MovieListUI.main(new String[]{""+movies.size()});
+    public static void listMovies() {
+        MovieListUI.view(movies);
     }
 
-    public static void viewMovieDetails(int index) {
-        Movie movie = movies.get(index-1);
-        System.out.println(movie);
-        MovieDetailsUI.setMovie(movie);
-        MovieDetailsUI.main(null);
+    public static void searchMovies() {
+        String search = InputController.getString("Enter title of movie to search for:");
+        List<Movie> searchResults = new ArrayList<>();
+        for (Movie movie : movies) {
+            if (movie.getTitle().toLowerCase().contains(search.toLowerCase())) 
+                searchResults.add(movie);
+        }
+        if (searchResults.isEmpty()) {
+            System.out.println("No movies found. Returning to homepage");
+            return;
+        }
+        System.out.println("Search results: ");
+        MovieListUI.view(searchResults);
     }
 
     public static void loadMovies() {
