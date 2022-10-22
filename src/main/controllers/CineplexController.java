30be3ecc3688ee697b9d8917dc6bc5975806aa7c
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-23 03:59:47 +0800

    viewShowTimes

--- src/main/controllers/CineplexController.java
+++ src/main/controllers/CineplexController.java
@@ -3,48 +3,44 @@ package main.controllers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import main.models.Cinema;
 import main.models.Cineplex;
 import main.models.Movie;
 import main.models.Session;
+import main.ui.ShowTimesUI;
 
 public class CineplexController extends Controller {
-    private static Cineplex[] cineplexes;
+    private static List<Cineplex> cineplexes;
     private static final String FILENAME = "src/main/data/cineplexes.ser";
 
     public static void viewShowTimes(Movie movie) {
+        List<List<Session>> movieSessions = new ArrayList<>();
         for (Cineplex cineplex : cineplexes) {
             for (Cinema cinema : cineplex.getCinemas()) {
-                List<Session> sessions = cinema.getShowTimes(movie);
-                if (sessions.isEmpty())
+                List<Session> cinemaSessions = cinema.getShowTimes(movie);
+                if (cinemaSessions.isEmpty())
                     continue;
-                System.out.println(cinema);
-                for (Session session : sessions) {
-                    LocalDateTime dateTime = session.getDateTime();
-                    System.out.println(dateTime.getDayOfMonth() + " " + dateTime.getMonth() + " " + dateTime.getYear() + " " + dateTime.toLocalTime() + " - " + session.getCinemaClass());
-                }
-                System.out.println();
+                movieSessions.add(cinemaSessions);
             }
         }
+        ShowTimesUI.view(movieSessions);
     }
 
     public static void loadCineplexes() {
         try {
             FileInputStream fileIn = new FileInputStream(FILENAME);
             ObjectInputStream in = new ObjectInputStream(fileIn);
-            cineplexes = (Cineplex[])in.readObject();
+            cineplexes = (List<Cineplex>)in.readObject();
             in.close();
             fileIn.close();
         } catch (IOException i) {
             i.printStackTrace();
-            return;
         } catch (ClassNotFoundException c) {
             System.out.println("Class not found");
             c.printStackTrace();
-            return;
         }
     }
 }
