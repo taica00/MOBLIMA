78eaaf42934b99c9c001ebfc46e10a8cd6659227
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-23 04:10:48 +0800

    cinemaCode

--- src/main/models/Cinema.java
+++ src/main/models/Cinema.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Cinema implements java.io.Serializable {
     private static final long serialVersionUID = 2L;
     private String cineplex;
+    private String cinemaCode;
     private String location;
     private List<Session> showTimes;
 
@@ -15,6 +16,7 @@ public class Cinema implements java.io.Serializable {
         this.cineplex = cineplex;
         this.location = location;
         showTimes = new ArrayList<>();
+        generateCinemaCode();
     }
 
     public void addSession(Movie movie, String date, String time, String cinemaType) {
@@ -28,21 +30,36 @@ public class Cinema implements java.io.Serializable {
         return cineplex + " - " + location;
     }
 
-    public List<Session> getShowTimes() {
-        return showTimes;
+    private void generateCinemaCode() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cineplex.charAt(0));
+        String[] locationWords = location.split(" ");
+        if (locationWords.length == 2)
+            sb.append(locationWords[0].charAt(0) + "" + locationWords[1].charAt(1));
+        else
+            sb.append(location.substring(0, 2).toUpperCase());
+        cinemaCode = sb.toString();
     }
-    
+
     public List<Session> getShowTimes(Movie movie) {
         List<Session> sessions = new ArrayList<>();
         for (Session session : showTimes) {
-            if (session.getMovie().getTitle().equals(movie.getTitle()))
+            if (session.getMovie().getTitle().equals(movie.getTitle()) && session.getDateTime().isAfter(LocalDateTime.now()))
                 sessions.add(session);
         }
         return sessions;
     }
+    
+    public List<Session> getShowTimes() {
+        return showTimes;
+    }
 
     public String getLocation() {
         return location;
+    }
+
+    public String getCinemaCode() {
+        return cinemaCode;
     }
 
     public String getCineplex() {
