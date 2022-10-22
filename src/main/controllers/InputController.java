7dc3d4215bcd119234034191ab91a35cf22227b5
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-23 04:00:42 +0800

    getInt check for valid input

--- src/main/controllers/InputController.java
+++ src/main/controllers/InputController.java
@@ -5,11 +5,30 @@ import java.util.Scanner;
 public class InputController extends Controller {
     private static Scanner scan = new Scanner(System.in);
 
-    public static int getInt() {
-        return scan.hasNextInt() ? scan.nextInt() : -1;
+    public static int getInt(int start, int end, String message) {
+        boolean validInput = false;
+        int choice = -1;
+        while (!validInput) {    
+            System.out.print(message);
+            if (scan.hasNextInt())
+                choice = scan.nextInt();
+            else
+                clear();
+            if (choice >= start && choice <= end)
+                validInput = true;
+            else
+                System.out.println("Invalid input. Please try again\n");
+        }
+        return choice;
     }
 
-    public static void clear() {
+    public static String getString(String message) {
+        clear();
+        System.out.println(message);
+        return scan.nextLine();
+    }
+
+    private static void clear() {
         scan.nextLine();
     }
 
