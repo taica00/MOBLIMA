d8e9d6b41b7604eac31d79182ffb7282ec82a719
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-25 05:03:49 +0800

    remove

--- src/main/models/Ticket.java
+++
@@ -1,9 +0,0 @@
-package main.models;
-
-public class Ticket implements java.io.Serializable {
-    private static final long serialVersionUID = 7L;
-    private Cinema cinema;
-    private Session movieSession;
-    private double price;
-    private String seat;
-}
