b1c5cd57f1c87ffe129d542d164fb4a85c160193
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-25 23:54:16 +0800

    includes ticketprices

--- src/main/models/CinemaClass.java
+++ src/main/models/CinemaClass.java
@@ -5,81 +5,101 @@ public enum CinemaClass implements SeatingLayout {
         @Override
         public int rows() {return 17;}
         @Override
-        public int columns() {return 36;}
+        public int columns() {return 34;}
         @Override
-        public int[] aisles() {return new int[]{7, 28};}    
+        public int[] aisles() {return new int[]{7, 27};}
+        @Override
+        public double[] ticketPrices() {return new double[]{5, 7, 10, 14.5};}    
     },
     GVMAX("GVMax") {
         @Override
         public int rows() {return 10;}
         @Override
-        public int columns() {return 20;}
+        public int columns() {return 18;}
         @Override
-        public int[] aisles() {return new int[]{4, 15};}
+        public int[] aisles() {return new int[]{4, 14};}
+        @Override
+        public double[] ticketPrices() {return new double[]{10, 10, 11.5, 16};}    
     },
     GOLD_CLASS("Gold Class") {
         @Override
         public int rows() {return 4;}
         @Override
-        public int columns() {return 11;}
+        public int columns() {return 6;}
         @Override
-        public int[] aisles() {return new int[]{2, 5, 8};}
+        public int[] aisles() {return new int[]{2, 4};}
+        @Override
+        public double[] ticketPrices() {return new double[]{32, 32, 35, 45};}    
     },
     GOLD_CLASS_EXPRESS("Gold Class Express") {
         @Override
         public int rows() {return 4;}
         @Override
-        public int columns() {return 11;}
+        public int columns() {return 8;}
         @Override
-        public int[] aisles() {return new int[]{2, 5, 8};}
+        public int[] aisles() {return new int[]{2, 4, 6};}
+        @Override
+        public double[] ticketPrices() {return new double[]{28, 28, 31, 41};}    
     },
     DELUXE_PLUS("Deluxe Plus") {
         @Override
         public int rows() {return 5;}
         @Override
-        public int columns() {return 11;}
+        public int columns() {return 8;}
         @Override
-        public int[] aisles() {return new int[]{2, 5, 8};}
+        public int[] aisles() {return new int[]{2, 4, 6};}
+        @Override
+        public double[] ticketPrices() {return new double[]{10, 12, 17, 22};}    
     },
     IMAX("IMAX") {
         @Override
         public int rows() {return 8;}
         @Override
-        public int columns() {return 23;}
+        public int columns() {return 21;}
         @Override
-        public int[] aisles() {return new int[]{4, 18};}
+        public int[] aisles() {return new int[]{4, 16};}
+        @Override
+        public double[] ticketPrices() {return new double[]{17, 19, 20, 23};}    
     },
     LUMIERE("Lumiere") {
         @Override
         public int rows() {return 4;}
         @Override
-        public int columns() {return 8;}
+        public int columns() {return 6;}
         @Override
-        public int[] aisles() {return new int[]{2, 5};}
+        public int[] aisles() {return new int[]{2, 4};}
+        @Override
+        public double[] ticketPrices() {return new double[]{17, 18, 23, 28};}    
     },
     DREAMERS("Dreamers") {
         @Override
         public int rows() {return 6;}
         @Override
-        public int columns() {return 11;}
+        public int columns() {return 10;}
         @Override
         public int[] aisles() {return new int[]{5};}
+        @Override
+        public double[] ticketPrices() {return new double[]{7, 8, 9, 14};}    
     },
     PREMIERE("premiere") {
         @Override
         public int rows() {return 6;}
         @Override
-        public int columns() {return 11;}
+        public int columns() {return 8;}
         @Override
-        public int[] aisles() {return new int[]{2, 5, 8};}
+        public int[] aisles() {return new int[]{2, 4, 6};}
+        @Override
+        public double[] ticketPrices() {return new double[]{22, 25, 30, 35};}    
     },
     PLATINUM_MOVIE_SUITES("Platinum Movie Suites") {
         @Override
         public int rows() {return 5;}
         @Override
-        public int columns() {return 8;}
+        public int columns() {return 6;}
         @Override
-        public int[] aisles() {return new int[]{2, 5};}
+        public int[] aisles() {return new int[]{2, 4};}
+        @Override
+        public double[] ticketPrices() {return new double[]{16, 16, 25, 25};}    
     };
 
     private String string;
