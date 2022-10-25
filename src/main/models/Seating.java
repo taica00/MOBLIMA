14f3b2c80ab30b639416128d33a1386b7ee34795
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-25 23:55:14 +0800

    seats generation logic

--- src/main/models/Seating.java
+++ src/main/models/Seating.java
@@ -1,12 +1,29 @@
 package main.models;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Seating implements java.io.Serializable {
     private static final long serialVersionUID = 5L;
     private int[][] seats;
-    private int numAvailableSeats;
+    private Set<Integer> aisles;
 
     public Seating(CinemaClass cinemaClass) {
-        generateSeating(cinemaClass);
+        if (cinemaClass == null)
+            throw new IllegalArgumentException("cinemaClass cannot be null");
+        int rows = cinemaClass.rows();
+        int columns = cinemaClass.columns();
+        aisles = new HashSet<>(Arrays.stream(cinemaClass.aisles()).boxed().collect(Collectors.toList()));
+        seats = new int[rows][columns];
+    }
+
+    public boolean bookSeat(char row, char col) {
+        if (seats[row-'A'][col-'1'] == 1) // occupied seat
+            return false;
+        seats[row-'A'][col-'1'] = 1;
+        return true;
     }
 
     @Override
@@ -14,55 +31,36 @@ public class Seating implements java.io.Serializable {
         StringBuilder sb = new StringBuilder();
         sb.append("SEATS SELECTION:\n\n");
         sb.append(" ");
-        for (int i = 0; i < seats[0].length-3; i++) {
-            if (i == (seats[0].length-5)/2)
-                sb.append("-----SCREEN-");
+        int rowLength = seats[0].length + aisles.size();
+        for (int i = 0; i < rowLength-2 ; i++) {
+            if (i == (rowLength-2)/2)
+                sb.append("-SCREEN-");
             else
                 sb.append("---");
         }
         sb.append("--\n\n");
         for (int i = 0; i < seats.length; i++) {  
-            sb.append((char)(i+'A') + " ");  // index
-            for (int j = 0; j < seats[i].length; j++) { //seats
+            sb.append((char)(i+'A') + " ");  // row index
+            for (int j = 0; j < seats[i].length; j++) { // seats
+                if (aisles.contains(j)) 
+                    sb.append("   ");    
                 if (seats[i][j] == 0)
                     sb.append("[ ]");
-                else if (seats[i][j] == 1)
-                    sb.append("[X]");
-                else
-                    sb.append("   ");
+                else 
+                    sb.append("[X]");   
             }
-            sb.append(" " + (char)(i+'A') + "\n"); // index
+            sb.append(" " + (char)(i+'A') + "\n"); // row index
         }
         sb.append("  ");
-        for (int i = 0, j = 1; i < seats[0].length; i++) { // index
-            if (seats[0][i] == -1)
-                sb.append("   ");
-            else { 
-                if (j >= 10)
-                    sb.append(j + " ");
-                else
-                    sb.append(" " + j + " ");
-                j++;
-            }
+        for (int i = 1; i <= seats[0].length; i++) { // col index
+            if (aisles.contains(i-1)) 
+                sb.append("   ");  
+            if (i >= 10)
+                sb.append(i + " ");
+            else
+                sb.append(" " + i + " ");
         }
         sb.append("\nLegend: [ ]Available  [X]Occupied\n"); // legend
         return sb.toString();
-    }
-
-    private void generateSeating(CinemaClass cinemaClass) {
-        int rows = cinemaClass.rows();
-        int columns = cinemaClass.columns();
-        int[] aisles = cinemaClass.aisles();
-        seats = new int[rows][columns];
-        for (int i : aisles) {
-            for (int j = 0; j < seats.length; j++) {
-                seats[j][i] = -1; //aisle
-            }
-        }
-        numAvailableSeats = rows * (columns - aisles.length); 
-    }
-
-    public int getNumAvailableSeats() {
-        return numAvailableSeats;
     }
 }
