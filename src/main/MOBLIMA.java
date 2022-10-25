c4a972ec105e019c5a6182f52c8aeaf8866d63b3
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-25 23:53:20 +0800

    initialise moviegoers and holidays

--- src/main/MOBLIMA.java
+++ src/main/MOBLIMA.java
@@ -1,6 +1,8 @@
 package main;
 import main.controllers.CineplexController;
 import main.controllers.MovieController;
+import main.controllers.PricingController;
+import main.controllers.TransactionsController;
 import main.ui.MainMenuUI;
 
 public class MOBLIMA {
@@ -13,10 +15,14 @@ public class MOBLIMA {
     private static void loadState() {
         CineplexController.loadCineplexes();
         MovieController.loadMovies();
+        TransactionsController.loadMovieGoers();
+        PricingController.loadHolidays();
     }
 
     private static void saveState() {
         CineplexController.saveCineplexes();
         MovieController.saveMovies();
+        TransactionsController.saveMovieGoers();
+        PricingController.saveHolidays();
     }
 }
