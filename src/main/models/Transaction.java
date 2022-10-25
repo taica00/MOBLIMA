141f1e507cd9e1c2d9b59e0935e6e5fd27f08fe0
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-25 23:55:39 +0800

    transaction class

--- src/main/models/Transaction.java
+++ src/main/models/Transaction.java
@@ -1,7 +1,23 @@
 package main.models;
 
 public class Transaction implements java.io.Serializable {
-    private static final long serialVersionUID = 8L;
+    private static final long serialVersionUID = 7L;
+    private String name;
+    private String mobileNumber;
+    private String emailAddress;
+    private String TID;
+    private Session session;
+    private String[] seats;
+    private double price;
 
+    public Transaction() {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
     
 }
