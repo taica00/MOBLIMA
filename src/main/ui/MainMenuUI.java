cbd1b43ea34282f3953c91e8d08744dd7f435a97
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-30 01:25:21 +0800

    numbering

--- src/main/ui/MainMenuUI.java
+++ src/main/ui/MainMenuUI.java
@@ -1,8 +1,8 @@
 package main.ui;
 
+import main.controllers.CineplexController;
 import main.controllers.InputController;
 import main.controllers.MovieController;
-import main.controllers.TransactionsController;
 
 public class MainMenuUI extends UI {
     public static void view() {
@@ -12,24 +12,25 @@ public class MainMenuUI extends UI {
             System.out.println("1. List all movies.");
             System.out.println("2. Search for a movie.");
             System.out.println("3. View showtimes.");
-            System.out.println("3. View booking history");
-            System.out.println("4. List Top 5 ranking movies by ticket sales");
-            System.out.println("5. List Top 5 ranking movies by reviewers' ratings");
-            System.out.println("6. Admin login");
-            System.out.println("7. Exit");
-            choice = InputController.getInt(1, 7, "Select an option: ");
+            System.out.println("4. View booking history");
+            System.out.println("5. List Top 5 ranking movies by ticket sales");
+            System.out.println("6. List Top 5 ranking movies by reviewers' ratings");
+            System.out.println("7. Admin login");
+            System.out.println("8. Exit");
+            choice = InputController.getInt(1, 8, "Select an option: ");
             System.out.println();
             
             switch(choice) {
                 case 1: MovieController.listMovies(); break;
                 case 2: MovieController.searchMovies(); break;
-                case 3: break;
-                case 4: TransactionsController.viewTransactions(); break;
+                case 3: CineplexController.displayCinemas(); break;
+                case 4: BookingHistoryUI.view(); break;
                 case 5: break;
                 case 6: break; 
                 case 7: break;
+                case 8: break;
                 default: System.out.println("Something weird happened");
             }
-        } while (choice != 7);
+        } while (choice != 8);
     }
 }
