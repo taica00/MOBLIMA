622db9a500f315b51c09a891848c6229f785c6a4
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-30 01:20:21 +0800

    populate moviegoers.ser

---
+++ src/main/utils/PopulateMovieGoers.java
@@ -0,0 +1,16 @@
+package main.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.models.MovieGoer;
+
+public class PopulateMovieGoers extends Populator {
+    public static void main(String[] args) {
+        List<MovieGoer> movieGoers = new ArrayList<>();
+        movieGoers.add(new MovieGoer("Mike Coxlong", "98765432", "mikecoxlong@gmail.com", "7480932833"));
+        movieGoers.add(new MovieGoer("Joe", "98765431", "joe@gmail.com", "7483934833"));
+        movieGoers.add(new MovieGoer("Elon Musk", "98765433", "elonmusk@gmail.com", "7482132833"));
+        serialize(movieGoers, "moviegoers.ser");
+    }
+}
