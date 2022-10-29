162c23e045d6c47c69a1b714d7dddd9c441b57ea
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-30 01:17:14 +0800

    view seating for session list of cinema

--- src/main/controllers/SessionController.java
+++ src/main/controllers/SessionController.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.models.Cinema;
 import main.models.CinemaClass;
 import main.models.Session;
 import main.ui.BookingUI;
@@ -22,6 +23,18 @@ public class SessionController extends Controller {
                     SeatingUI.view(session);
                     return;
                 }
+            }
+        }
+        System.out.println("Session not found. Returning to homepage.");
+    }
+
+    public static void viewSeating(Cinema cinema, String movie, String date, String cinemaClass, String time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyHHmm");
+        LocalDateTime dateTime = LocalDateTime.parse(date+time, formatter);
+        for (Session session : cinema.getSessions()) {
+            if (movie.equalsIgnoreCase(session.getMovie().getTitle()) &&  dateTime.isEqual(session.getDateTime()) && CinemaClass.valueOf(cinemaClass).equals(session.getCinemaClass())) {
+                SeatingUI.view(session);
+                return;
             }
         }
         System.out.println("Session not found. Returning to homepage.");
