512279c6cdca7e470ad0293637beff75789145d5
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-29 15:38:07 +0800

    getNumericString, getBoolean, confirmIdentity

--- src/main/controllers/InputController.java
+++ src/main/controllers/InputController.java
@@ -2,30 +2,70 @@ package main.controllers;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
+import main.models.MovieGoer;
+
 public class InputController extends Controller {
     private static Scanner scan = new Scanner(System.in);
 
     public static int getInt(int start, int end, String message) {
-        boolean validInput = false;
-        int choice = -1;
-        while (!validInput) {    
+        while (true) {
+            clear();    
             System.out.print(message);
-            if (scan.hasNextInt())
-                choice = scan.nextInt();
-            else
-                clear();
-            if (choice >= start && choice <= end)
-                validInput = true;
-            else
-                System.out.println("Invalid input. Please try again\n");
+            if (scan.hasNextInt()) {
+                int choice = scan.nextInt();
+                if (choice >= start && choice <= end)
+                    return choice;
+            }
+            System.out.println("Invalid input. Please try again\n");
         }
-        return choice;
     }
 
     public static String getString(String message) {
         clear();
         System.out.print(message);
         return scan.nextLine();
+    }
+
+    public static String getNumericString(String message, int lo, int hi) {
+        while (true) {
+            clear();
+            System.out.print(message);
+            String input = scan.nextLine();
+            if (StringUtils.isNumeric(input) || (input.length() >= lo && input.length() <= hi))
+                return input;
+            System.out.println("Invalid input. Please try again");
+        }
+    }
+
+    public static boolean getBoolean(String message, char T, char F) {
+        while (true) {
+            clear();
+            System.out.print(message);
+            String choice = scan.next();
+            if (choice.length() == 1) {
+                if (choice.charAt(0) == T)
+                    return true;
+                else if (choice.charAt(0) == F)
+                    return false;
+            }
+            System.out.println("Invalid input. Please try again");
+        }
+    }
+
+    public static boolean confirmIdentity(MovieGoer movieGoer) {
+        while (true) {
+            String name = InputController.getString("Enter name: ");
+            String mobile = InputController.getString("Enter mobile number: ");
+            if (name.equals(movieGoer.getName()) && mobile.equals(movieGoer.getMobile()))
+                return true;
+            else {
+                System.out.println("Invalid particulars.");
+                if (!getBoolean("Try again? (Y/N)", 'Y', 'N'))
+                    return false;
+            }
+        }
     }
 
     private static void clear() {
