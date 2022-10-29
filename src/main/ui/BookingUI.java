d7de53e7808ee8067074472287821f80eda52167
taica00 <64315786+taica00@users.noreply.github.com>
2022-10-29 15:39:20 +0800

    methods from input controller

--- src/main/ui/BookingUI.java
+++ src/main/ui/BookingUI.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import main.controllers.InputController;
 import main.controllers.PricingController;
+import main.controllers.SessionController;
 import main.controllers.TransactionsController;
 import main.models.MovieGoer;
 import main.models.Session;
@@ -18,22 +19,32 @@ public class BookingUI extends UI {
         int seniors = 0;
         int students = 0;
         int numTickets = bookedSeats.size();
-        if (PricingController.eligibleForConcession(session.getDateTime())) {
+        if (PricingController.eligibleForConcession(session.getDateTime())) { // showtime eligible for student/senior price
             seniors = InputController.getInt(0, numTickets, "Enter number of senior citizens: ");
             students = InputController.getInt(0, numTickets-seniors, "Enter number of students: ");
         }
         double price = PricingController.getTicketsPrice(session, numTickets, seniors, students);
-        System.out.printf("Grand Total: %.2f\n", price);
+        System.out.printf("Grand Total: %.2f%n", price);
         String email = InputController.getString("Enter email address: ");
-        MovieGoer movieGoer = TransactionsController.getMovieGoer(email);
-        if (movieGoer != null) {
+        MovieGoer movieGoer = TransactionsController.getMovieGoer(email); // use email to identify movie goer
+        if (movieGoer != null) {  // moviegoer has booked movie(s) before
             System.out.println("We have records of your past transactions. Please confirm your identity.");
-            String name = InputController.getString("Enter name: ");
-            String mobile = InputController.getString("Enter mobile number: ");
+            if (!InputController.confirmIdentity(movieGoer)) {  // unable to confirm identity, booking unsuccessful
+                SessionController.undoBooking(session, bookedSeats);  // mark seats back as available
+                System.out.println("Returning home.");
+                return;
+            }
+            String message = "Would you like to use your existing payment method " + movieGoer.getMaskedCardNumber() + "? (Y/N)";
+            if (!InputController.getBoolean(message, 'Y', 'N'))
+                movieGoer.setCardNumber(InputController.getNumericString("Enter card number: ", 8, 19)); // use new payment method
         }
-        String name = InputController.getString("Enter name: ");
-        String mobile = InputController.getString("Enter mobile number: ");
-        
-        TransactionsController.makePayment(session, bookedSeats, name, mobile, email, price);
+        else { // moviegoer is a first timer
+            String name = InputController.getNumericString("Enter name: ", 1, 50);
+            String mobile = InputController.getNumericString("Enter mobile number: ", 8, 15);
+            String cardNumber = InputController.getNumericString("Enter card number: ", 8, 19);
+            movieGoer = new MovieGoer(name, mobile, email, cardNumber);
+            TransactionsController.addMovieGoer(movieGoer);
+        }
+        TransactionsController.makePayment(session, bookedSeats, movieGoer, price); // generate transaction
     }
 }
