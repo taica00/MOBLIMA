7bc478eefd0d5ef90b841cbeed4efc8fcfb74c3c
taica00 <64315786+taica00@users.noreply.github.com>
2022-11-04 01:23:20 +0800

    UI to update ticket prices

---
+++ src/main/ui/UpdatePrices.java
@@ -0,0 +1,20 @@
+package main.ui;
+
+import main.controllers.InputController;
+import main.models.CinemaClass;
+
+public class UpdatePrices extends UI {
+    public static void view() {
+        String cinemaClassString = InputController.getString("Enter cinema class (replace space with underscore): ");
+        CinemaClass cinemaClass = CinemaClass.valueOf(cinemaClassString.toUpperCase());
+        System.out.println("Select pricing category to change: ");
+        System.out.println("1. Senior Citizen (Mon-Fri before 6pm)");
+        System.out.println("2. Student (Mon-Fri before 6pm)");
+        System.out.println("3. Non-peak (Mon-Thurs)");
+        System.out.println("4. Peak (Fri-Sun/PH)");
+        int choice = InputController.getInt(1, 4, "Enter your option: ");
+        double price = InputController.getDouble("Enter new price:");
+        cinemaClass.setPrice(choice-1, price);
+        System.out.println("Pricing updated.");
+    }
+}
