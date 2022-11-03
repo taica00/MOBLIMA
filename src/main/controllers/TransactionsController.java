710faa7d685609fd062a8fd9303e53f32b712845
taica00 <64315786+taica00@users.noreply.github.com>
2022-11-04 02:53:17 +0800

    keep track of ticket sales of each movie

--- src/main/controllers/TransactionsController.java
+++ src/main/controllers/TransactionsController.java
@@ -1,17 +1,22 @@
 package main.controllers;
 
 import java.util.List;
+import java.util.Map;
+
+import main.models.Movie;
 import main.models.MovieGoer;
 import main.models.Session;
 import main.models.Transaction;
 
 public class TransactionsController extends Controller {
     private static List<MovieGoer> movieGoers;
-    private static final String FILEPATH = "src/main/data/moviegoers.ser";
+    private static Map<Movie, Integer> ticketSales;
+    private static final String FILEPATH = "src/main/data/";
 
     public static void makePayment(Session session, List<String> bookedSeats, MovieGoer movieGoer, double price) {
         Transaction transaction = new Transaction(session, bookedSeats, price);
         movieGoer.addTransaction(transaction);
+        addTicketSales(session.getMovie(), transaction.getNumTickets());
     }
 
     public static MovieGoer getMovieGoer(String email) {
@@ -26,12 +31,29 @@ public class TransactionsController extends Controller {
         movieGoers.add(movieGoer);
     }
 
+    public static Map<Movie, Integer> getTicketSales() {
+        return ticketSales;
+    }
+
+    private static void addTicketSales(Movie movie, int numTickets) {
+        int sales = ticketSales.getOrDefault(movie, 0);
+        sales += numTickets;
+        ticketSales.put(movie, sales);
+    }
+
     public static void loadMovieGoers() {
-        movieGoers = loadData(FILEPATH);
+        movieGoers = loadData(FILEPATH + "moviegoers.ser");
     }
 
     public static void saveMovieGoers() {
-        saveData(movieGoers, FILEPATH);
+        saveData(movieGoers, FILEPATH + "moviegoers.ser");
+    }
 
+    public static void loadTicketSales() {
+        ticketSales = loadData(FILEPATH + "ticketsales.ser");
+    }
+
+    public static void saveTicketSales() {
+        saveData(ticketSales, FILEPATH + "ticketsales.ser");
     }
 }
