1d4e393aba98d30cc7691d91f8f7b7fb42994bc9
taica00 <64315786+taica00@users.noreply.github.com>
2022-11-06 22:56:42 +0800

    constant string value for invalid input

--- src/main/controllers/InputController.java
+++ src/main/controllers/InputController.java
@@ -8,7 +8,15 @@ import main.models.MovieGoer;
 
 public class InputController extends Controller {
     private static Scanner scan = new Scanner(System.in);
+    private static final String INVALID_INPUT = "Invalid input. Please try again\n";
 
+    
+    /** 
+     * @param start
+     * @param end
+     * @param message
+     * @return int
+     */
     public static int getInt(int start, int end, String message) {
         while (true) {
             clear();    
@@ -18,16 +26,28 @@ public class InputController extends Controller {
                 if (choice >= start && choice <= end)
                     return choice;
             }
-            System.out.println("Invalid input. Please try again\n");
+            System.out.println(INVALID_INPUT);
         }
     }
 
+    
+    /** 
+     * @param message
+     * @return String
+     */
     public static String getString(String message) {
         clear();
         System.out.print(message);
         return scan.nextLine();
     }
 
+    
+    /** 
+     * @param message
+     * @param lo
+     * @param hi
+     * @return String
+     */
     public static String getNumericString(String message, int lo, int hi) {
         while (true) {
             clear();
@@ -35,10 +55,17 @@ public class InputController extends Controller {
             String input = scan.nextLine();
             if (StringUtils.isNumeric(input) || (input.length() >= lo && input.length() <= hi))
                 return input;
-            System.out.println("Invalid input. Please try again");
+            System.out.println(INVALID_INPUT);
         }
     }
 
+    
+    /** 
+     * @param message
+     * @param T
+     * @param F
+     * @return boolean
+     */
     public static boolean getBoolean(String message, char T, char F) {
         while (true) {
             clear();
@@ -50,21 +77,31 @@ public class InputController extends Controller {
                 else if (choice.charAt(0) == F)
                     return false;
             }
-            System.out.println("Invalid input. Please try again");
+            System.out.println(INVALID_INPUT);
         }
     }
 
+    
+    /** 
+     * @param message
+     * @return double
+     */
     public static double getDouble(String message) {
         while (true) {
             clear();
             System.out.print(message);
             Double input = scan.nextDouble();
             if (input < 0)
-                System.out.println("Invalid input. Please try again");
+                System.out.println(INVALID_INPUT);
             return input;
         }
     }
 
+    
+    /** 
+     * @param movieGoer
+     * @return boolean
+     */
     public static boolean confirmIdentity(MovieGoer movieGoer) {
         while (true) {
             String name = InputController.getString("Enter name: ");
@@ -82,8 +119,4 @@ public class InputController extends Controller {
     private static void clear() {
         scan = new Scanner(System.in);
     }
-
-    
-
-
 }
